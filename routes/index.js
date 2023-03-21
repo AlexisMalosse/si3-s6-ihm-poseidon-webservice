534298e746dd848e43e9ee277e4c9df1ed89746c
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-03-21 22:11:29 +0100

    feat: add robots.txt

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const pois = require('./pois');
+const robots = require('./robots');
 
 module.exports = (app) => {
     app.use('/pois', pois);
+    app.use('/robots.txt', robots);
     app.use('*', (req, res) => {
         res.send('Not found!!!');
     });
