cba89f2f568806122d12bb4cc234fb9544827e4b
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-03-21 22:40:49 +0100

    feat: added https

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = require('./app');
 const config = require('./config');
+const fs = require('fs');
+const https = require('https');
 
 const PORT = process.env.PORT || config.port;
 
-const server = app.listen(PORT, () => {
-    console.log('server is running on port', server.address().port);
-});
+try {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.poseidon.alexismalosse.fr/privkey.pem');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/api.poseidon.alexismalosse.fr/fullchain.pem');
+
+    https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(PORT);
+} catch (e) {
+    const server = app.listen(PORT, () => {
+        console.log('server is running on port', server.address().port);
+    });
+}
