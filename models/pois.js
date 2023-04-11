a29a66d3e601be8e477269fb47132d9f5c05075e
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-04-11 17:25:03 +0200

    feat: update pois weather conditon enum

--- models/pois.js
+++ models/pois.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
-const weatherEnum = ['sun', 'cloud', 'thunderstorm', 'wind', 'storm', 'rain'];
+const weatherEnum = {
+    SUN: 'SUN',
+    CLOUD: 'CLOUD',
+    THUNDERSTORM: 'THUNDERSTORM',
+    WIND: 'WIND',
+    STORM: 'STORM',
+    RAIN: 'RAIN',
+}
 
 const poisSchema = new mongoose.Schema(
     {
         latitude: { type: Number, required: true },
         longitude: { type: Number, required: true },
-        weather: { type: String, enum: weatherEnum, required: true },
+        weather: { type: String, enum: Object.values(weatherEnum), required: true },
         perimeter: { type: Number, required: true },
         finished: { type: Boolean, default: false },
     },
