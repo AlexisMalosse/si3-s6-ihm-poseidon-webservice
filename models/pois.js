a2530a27d86e6e5b3446e93c21a15222cddd88d3
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-04-11 15:53:21 +0200

    feat: add timestamp to pois

--- models/pois.js
+++ models/pois.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
-const poisSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
-});
+const poisSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+    },
+    {
+        timestamps: true
+    },
+);
 
 module.exports = mongoose.model('Pois', poisSchema);
