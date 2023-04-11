19e88d7803b8f0019eadde2626fc6ee283a6ef79
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-04-11 15:48:24 +0200

    fix: get all pois get route

--- services/poisService.js
+++ services/poisService.js
@@ -1,6 +1,9 @@
 const Pois = require('../models/pois');
 
-exports.getAllPois = () => Pois.find();
+exports.getAllPois = async () => {
+    const pois = await Pois.find();
+    return pois.map(poi => poi.toObject());
+};
 
 exports.createPoi = (poi) => {
     const newPoi = new Pois(poi);
