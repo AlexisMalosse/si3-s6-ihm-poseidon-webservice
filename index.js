eec7d6b3c41658c77935f3e9f9b0866ac27d26f9
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-06-08 22:14:24 +0200

    feat: clean code

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const app = require('./app');
 const config = require('./config');
-const fs = require('fs');
-const https = require('https');
 
 const PORT = process.env.PORT || config.port;
 
-try {
-    const privateKey = fs.readFileSync('REMOVE_HERE/etc/letsencrypt/live/api.poseidon.alexismalosse.fr/privkey.pem');
-    const certificate = fs.readFileSync('REMOVE_HERE/etc/letsencrypt/live/api.poseidon.alexismalosse.fr/fullchain.pem');
-
-    https.createServer({
-        key: privateKey,
-        cert: certificate
-    }, app).listen(PORT);
-    console.log('https server is running on port ' + PORT);
-} catch (e) {
-    console.log(e);
-    const server = app.listen(PORT, () => {
-        console.log('server is running on port', server.address().port);
-    });
-}
+const server = app.listen(PORT, () => {
+    console.log('server is running on port', server.address().port);
+});
